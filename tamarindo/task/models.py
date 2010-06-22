c1dfe306a66147739c4461352f498817f965d7cb
Pablo Astigarraga <poteland@gmail.com>
2010-06-21 22:09:32 -0300

    Added parent being shown in detail view

--- tamarindo/task/models.py
+++ tamarindo/task/models.py
@@ -3,9 +3,10 @@ from django.db import models
 class Task (models.Model):
     name = models.CharField(max_length=256)
     description = models.TextField()
-    completed = models.BooleanField()
+    #started_date = models.DateTimeField()
     parent = models.ForeignKey('self', blank=True, null=True, related_name='child_set')
-    
+    completed = models.BooleanField()
+    #completed_date = models.DateTimeField()
     
     def __unicode__(self):
         return self.name
