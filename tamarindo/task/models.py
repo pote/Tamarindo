ca15fcf6aeafc41546d1e4b1d5247f8a63b9c7af
Pablo Astigarraga <poteland@gmail.com>
2010-06-22 16:29:40 -0300

    starting with deadlines

--- tamarindo/task/models.py
+++ tamarindo/task/models.py
@@ -2,11 +2,13 @@ from django.db import models
 
 class Task (models.Model):
     name = models.CharField(max_length=256)
+    parent = models.ForeignKey('self', blank=True, null=True, related_name='child_set')    
     description = models.TextField()
+    deadline = models.DateTimeField(blank=True, null=True)
     started_date = models.DateTimeField()
-    parent = models.ForeignKey('self', blank=True, null=True, related_name='child_set')
     completed = models.BooleanField()
     completed_date = models.DateTimeField(blank=True, null=True)
+    
     
     def __unicode__(self):
         return self.name
