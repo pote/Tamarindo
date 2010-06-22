998abe9938e40d04f86231fb6e96c126167e105f
Pablo Astigarraga <poteland@gmail.com>
2010-06-21 21:40:03 -0300

    Enabled Task parent/child relationship and adjusted detailed view accordingly

--- tamarindo/task/models.py
+++ tamarindo/task/models.py
@@ -4,7 +4,8 @@ class Task (models.Model):
     name = models.CharField(max_length=256)
     description = models.TextField()
     completed = models.BooleanField()
-
-
+    parent = models.ForeignKey('self', blank=True, null=True, related_name='child_set')
+    
+    
     def __unicode__(self):
         return self.name
--- tamarindo/task/views.py
+++ tamarindo/task/views.py
@@ -9,7 +9,9 @@ def index(request):
     return render_to_response('index.html', {'task_list': task_list})
 
 def detail(request, task_id):
-    task = get_object_or_404(Task, pk=task_id)
-    return render_to_response('detail.html', {'task': task},
-                               context_instance=RequestContext(request))
-    
+    main_task = get_object_or_404(Task, pk=task_id)
+    child_tasks = []
+    for task in Task.objects.all():
+        if task.parent == main_task:
+            child_tasks.append(task)
+    return render_to_response('detail.html', {'main_task': main_task, 'child_tasks': child_tasks})
