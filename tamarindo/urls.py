c83c80b5f931221459467197f3e1a4100beaa9b8
Pablo Astigarraga <poteland@gmail.com>
2010-07-04 16:08:51 -0300

    Issue with serving static files

--- tamarindo/urls.py
+++ tamarindo/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls.defaults import *
 from django.contrib import admin
+from django.conf import settings
 admin.autodiscover()
 
 urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
     (r'^task/', include('tamarindo.task.urls')),
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
 )
